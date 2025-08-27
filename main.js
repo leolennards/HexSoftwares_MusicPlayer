7b0a585db481d2b5df02c095e7940161d6145530
Leo Lennards <leo.lukelennards@gmail.com>
2025-08-27 11:13:34 +0200

    Updated music player with new changes

--- main.js
+++ main.js
@@ -11,8 +11,6 @@ const songs = [
 
   { title: "All I Have (feat. LL Cool J)", src: "songs/Jennifer Lopez - All I Have (feat. LL Cool J).mp3", artist: "Jennifer Lopez", albumArt: "images/JenniferLopez.jpg" },
 
-  { title: "Baby (feat. Kerry)", src: "songs/Kulud-Baby-featKerry.mp3", artist: "Kulud", albumArt: "images/Baby.jpg" },
-
   { title: "Be My Queen (feat. Verd)", src: "songs/Kulud - Be My Queen (feat. Verd).mp3", artist: "Kulud", albumArt: "images/Queen.jpg" },
 
   { title: "Adorn", src: "songs/Miguel - Adorn (Official Video).mp3", artist: "Miguel", albumArt: "images/Adorn.jpg" },
